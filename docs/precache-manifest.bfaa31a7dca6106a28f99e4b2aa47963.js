7f4677fef4c43b84da1dd9b821192859a60ed782
nicolasmisseri <nicomisseri1@gmail.com>
2020-10-20 11:14:14 -0300

    docs con index ok

---
+++ docs/precache-manifest.bfaa31a7dca6106a28f99e4b2aa47963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7401cba5cf7efef677a087b619b112",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb47427d938808aec61a",
+    "url": "/static/css/main.aae85114.chunk.css"
+  },
+  {
+    "revision": "b7f984888fe32bb1a963",
+    "url": "/static/js/2.cfa476fb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cfa476fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb47427d938808aec61a",
+    "url": "/static/js/main.4d3834df.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
